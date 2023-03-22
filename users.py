aaf23afd1736c6519b7f4da8c8d1fe9792bcd7f8
Martinson <hosuatetteh@gmail.com>
2023-03-22 21:57:06 +0000

    broke down account dictionary into two separate dictionaries

--- users.py
+++ users.py
@@ -1,33 +1,38 @@
-accounts = {
-    'regular':
-    {
+regular = {
         'A_12345':
         {
             'name': 'name_1',
             'pass': 'A_12345',
+            'email': '',
             },
         'B_12345':
         {
             'name': 'name_2',
             'pass': 'B_12345',
+            'email': '',
+
             },
         'C_12345':
         {
             'name': 'ame_3',
             'pass': 'C_12345',
+            'email': '',
             },
         'D_12345':
         {
             'name': 'name_4',
             'pass': 'D_12345',
+            'email': '',
             },
         'E_12345':
         {
             'name': 'name_5',
             'pass': 'E_12345',
+            'email': '',
             }
-        },
-    'admin':
-        {}
-    },
+        }
+
+admin = {
+
+}
 
